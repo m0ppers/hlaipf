9643f95ccd22b51d852709358532a236e3c5f3bc
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-08-08 12:46:43 +0200

    IT WORKS :O

--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,44 @@ extern crate git2;
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate regex;
+extern crate time;
 
-use git2::{Repository, Error, Oid, Tree, ObjectType, Commit, Diff, DiffOptions};
+use git2::{Repository, Error, Oid, Tree, Commit, Diff, DiffOptions, Time};
 use docopt::Docopt;
 use std::fs;
-use std::path::{PathBuf, Path};
+use std::path::{PathBuf};
+use time::{Timespec, at_utc};
 
-fn diff_contains_php(repo: Repository, from: Tree, to: Tree) -> bool {
+fn diff_contains_php(repo: &Repository, from: Option<&Tree>, to: Option<&Tree>) -> bool {
     let mut opts = DiffOptions::new();
     
-    let fail_repo = Repository::open(repo.path());
-    if fail_repo.is_ok() {
-        let diff = Diff::tree_to_tree(&fail_repo.unwrap(), Some(&from), Some(&to), Some(&mut opts));
-        return diff.unwrap().deltas().any(|delta| delta.new_file().path().unwrap().to_str().unwrap().ends_with(".php"))
-    }
-    false
+    let diff = Diff::tree_to_tree(repo, from, to, Some(&mut opts));
+    return diff.unwrap().deltas().any(|delta| delta.new_file().path().unwrap().to_str().unwrap().ends_with(".php"))
 }
 
-fn is_php(repo: Repository, commit: Commit) -> bool {
-    println!("Parents: {}", commit.parents().count());
+fn is_php(repo: &Repository, commit: Commit) -> bool {
     match commit.parents().count() {
         // mop: todo..first commit. need to find out how to handle that
-        0 => false,
-        1 => diff_contains_php(repo, commit.parent(0).unwrap().tree().unwrap(), commit.tree().unwrap()),
+        0 => diff_contains_php(repo, None, Some(&commit.tree().unwrap())),
+        1 => diff_contains_php(repo, Some(&commit.parent(0).unwrap().tree().unwrap()), Some(&commit.tree().unwrap())),
         _ => false,
     }
-}
-
-fn find_php<'a>(repo: &'a Repository) -> Result<Option<Commit<'a>>, Error> {
-    return Ok(None);
-    /*let headrev = try!(repo.revparse_single("HEAD"));
-    let headoid = headrev.id();
-    
-    let mut revwalk = try!(repo.revwalk());
-    revwalk.set_sorting(git2::SORT_TIME);
-    revwalk.push(headoid);
-*/
-    /*for id in revwalk {
-        let commit = try!(repo.find_commit(id));
-        if commit.author().email().unwrap().contains("andreas.streichardt@gmail.com") {
-            println!("ID => {}, author => {}, tree_id => {}", id, commit.author(), commit.tree_id());
-            
-            //if is_php(repo, commit) {
-                return Ok(Some(commit))
-            //}
-        }
-    }
-    Ok(None)
-    */
 }
 
 // mop: a mess..it SHOULD be an abstract collection of repositories like "this is my project directory" "this is my github account" etc.
 // hardcoded to directory right now because of incompetence
 struct RepositoryCollection {
-    name: String,
     read_dir_iter: fs::ReadDir
 }
 
 impl RepositoryCollection {
     fn create(name: &str) -> Option<RepositoryCollection> {
-        let mut path_buf = PathBuf::from(name);
+        let path_buf = PathBuf::from(name);
         let metadata = fs::metadata(path_buf.as_path().to_str().unwrap());
         
         // mop: later: github, bitbucket etc
         if metadata.unwrap().is_dir() {
-            let path_ref = &path_buf.as_path();
             Some(RepositoryCollection {
-                name: name.to_string(),
                 read_dir_iter: fs::read_dir(path_buf.as_path()).unwrap(),
             })
         } else {
@@ -91,42 +62,62 @@ impl Iterator for RepositoryCollection {
     }
 }
 
-struct PhpCommitLocator {
+struct PhpCommitLocator<'a> {
+    author: &'a str,
     repo: Repository,
 }
 
-impl PhpCommitLocator {
-    fn new(repo: Repository) -> PhpCommitLocator {
+impl<'a> PhpCommitLocator<'a> {
+    fn new(author: &str, repo: Repository) -> PhpCommitLocator {
         PhpCommitLocator {
+            author: author,
             repo: repo,
         }
     }
 
-    fn fetch_earliest_php_commit(&mut self) -> Option<HlaipfResult> {
-        None
-    }
-}
+    fn fetch_earliest_php_commit(&mut self) -> Result<Option<HlaipfResult>, Error> {
+        let headrev = try!(self.repo.revparse_single("HEAD"));
+        let headoid = headrev.id();
 
-struct HlaipfResult<'repo> {
-    repo: Repository,
-    commit: Commit<'repo>,
-}
-
-impl<'repo> HlaipfResult<'repo> {
-    fn new(repo: Repository, commit: Commit) -> HlaipfResult {
-        HlaipfResult {
-            repo: repo,
-            commit: commit,
+        let mut revwalk = try!(self.repo.revwalk());
+        revwalk.set_sorting(git2::SORT_TIME);
+        revwalk.push(headoid);
+        for id in revwalk {
+            let commit = try!(self.repo.find_commit(id));
+            if commit.author().email().unwrap().contains(self.author) {
+                // moved value workaround :S must investigate :S probably easy to fix :S
+                let commit_id = commit.id();
+                let commit_time = commit.time();
+                let message;
+                if commit.message().is_some() {
+                    message = Some(commit.message().unwrap().to_string());
+                } else {
+                    message = None;
+                }
+                if is_php(&self.repo, commit) {
+                    return Ok(Some(HlaipfResult {
+                        repository_path: self.repo.path().to_path_buf(),
+                        commit_oid: commit_id,
+                        commit_time: commit_time,
+                        commit_message: message
+                    }))
+                }
+            }
         }
+        Ok(None)
     }
+}
 
-    fn is_php(&self) -> bool {
-        true
-    }
+struct HlaipfResult {
+    repository_path: PathBuf,
+    commit_message: Option<String>,
+    commit_time: Time,
+    commit_oid: Oid,
 }
 
 #[derive(RustcDecodable)]
 struct Args {
+    arg_author: String,
     arg_repositorieslocation: Vec<String>,
 }
 
@@ -140,51 +131,53 @@ fn run(args: &Args) -> Result<(), Error> {
         .filter(|repository_collection_opt| repository_collection_opt.is_some())
         .map(|repository_collection| repository_collection.unwrap())
     ;
-     
-    let results:Vec<HlaipfResult> = repository_collections.fold(Vec::new(), |mut results, repository_collection| {
+
+    let mut results:Vec<HlaipfResult> = repository_collections
+    .flat_map(|repository_collection| {
         repository_collection
-        .map(|repository| {
-            PhpCommitLocator::new(repository)
-        })
-        .map(|mut php_commit_locator: PhpCommitLocator| {
-            php_commit_locator.fetch_earliest_php_commit()
-        })
-        .filter(|result: &Option<HlaipfResult>| {
-            result.is_some()
-        })
-        .map(|result: Option<HlaipfResult>| {
-            result.unwrap()
-        })
-        .fold(results, |mut results, result: HlaipfResult| {
-            results.push(result);
-            results
-        })
+    })
+    .map(|repository| {
+        PhpCommitLocator::new(&args.arg_author, repository)
+    })
+    .map(|mut php_commit_locator: PhpCommitLocator| {
+        php_commit_locator.fetch_earliest_php_commit()
+    })
+    .filter(|result| {
+        result.is_ok()
+    })
+    .map(|result| {
+        result.unwrap()
+    })
+    .filter(|result: &Option<HlaipfResult>| {
+        result.is_some()
+    })
+    .map(|result: Option<HlaipfResult>| {
+        result.unwrap()
+    })
+    .collect();
+
+    results.sort_by(|a, b| {
+        b.commit_time.seconds().cmp(&a.commit_time.seconds())
     });
+    
+    if results.is_empty() {
+        println!("Well done. You seem to be PHP free");
+    } else {
+        let ref result = results[0];
+        let commit_time_spec = Timespec::new(result.commit_time.seconds(), 0);
+        println!("Your last PHP commit was at {} in repository {}. Commit Id: {}", at_utc(commit_time_spec).rfc3339(), result.repository_path.as_path().to_string_lossy(), result.commit_oid);
 
-    /*for repository_collection in repository_collections {
-        for repository in repository_collection {
-            let find_result = find_php(&repository);
-            if find_result.is_ok() {
-                // mop: XXX silent error :S
-                let php_commit = find_result.unwrap();
-                if php_commit.is_some() {
-                    let result = HlaipfResult { repo: &repository, commit: &php_commit.unwrap() };
-                    results.push(result);
-                }
-            }
+        if let Some(ref commit_message) = result.commit_message {
+            println!("===============================");
+            println!("{}", commit_message);
         }
-    }
-    */
-
-    for result in results.iter() {
-        println!("RESULT {}", result.commit.author())
     }
     Ok(())
 }
 
 fn main() {
     const USAGE: &'static str = "
-usage: hlaipf <repositorieslocation>...
+usage: hlaipf <author> <repositorieslocation>...
 ";
 
     let args = Docopt::new(USAGE).and_then(|d| d.decode())
